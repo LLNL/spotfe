813c5c228bed029a4a2a8bbef2e08749df892ee3
Pascal Aschwanden <aschwanden1@llnl.gov>
2018-10-03 10:54:50 -0700

    More error handling.

--- web/js/CallSpot.js
+++ web/js/CallSpot.js
@@ -15,36 +15,48 @@ ST.CallSpot = function() {
             }
         }).done(function(value) {
 
-            var spotReturnedValue = value.output.command_out;
-            var parsed = JSON.parse( spotReturnedValue );
+            if( value.error !== "" ) {
 
-            var newp = [];
+                error_( value.error );
 
-            for( var x in parsed ) {
+            } else {
 
-                if( newp.length < 4500 ) {
-                    newp.push(parsed[x][0]);
+                var spotReturnedValue = value.output.command_out;
+                var parsed = JSON.parse(spotReturnedValue);
+
+                var newp = [];
+
+                for (var x in parsed) {
+
+                    if (newp.length < 4500) {
+                        newp.push(parsed[x][0]);
+                    }
                 }
+
+                newp[0]['Code Builder'] = "Filler0";
+                newp[1]['Code Builder'] = "Filler1";
+                newp[2]['Compiler Name'] = "GNU Filler";
+                newp[3]['Compiler Name'] = "GNU Filler";
+                newp[4]['Compiler Name'] = "GNU Filler";
+                newp[5]['Compiler Name'] = "GNU Filler";
+                newp[6]['Compiler Name'] = "GNU Filler";
+                newp[7]['Compiler Name'] = "GNU Filler";
+
+                ST.ReturnedDataStub.data = newp;
+                console.dir(newp);
+                RenderChartCollection();
             }
-
-            newp[0]['Code Builder'] = "Filler0";
-            newp[1]['Code Builder'] = "Filler1";
-            newp[2]['Compiler Name'] = "GNU Filler";
-            newp[3]['Compiler Name'] = "GNU Filler";
-            newp[4]['Compiler Name'] = "GNU Filler";
-            newp[5]['Compiler Name'] = "GNU Filler";
-            newp[6]['Compiler Name'] = "GNU Filler";
-            newp[7]['Compiler Name'] = "GNU Filler";
-
-            ST.ReturnedDataStub.data = newp;
-            console.dir(newp);
-            RenderChartCollection();
         }).error(function() {
 
             var link = "https://rzlc.llnl.gov/";
-
-            $('body').prepend('<div class="error_statement">Could not contact rzlc.llnl.gov  Make sure you have internet connection and are already authenticated with RZ.  For example <a target="_blank" href="' + link + '">RZ Link</a> </div>');
+            error_('Could not contact rzlc.llnl.gov  Make sure you have internet connection and are already authenticated with RZ.  For example <a target="_blank" href="' + link + '">RZ Link</a>');
         });
+    };
+
+    var error_ = function( str ) {
+
+        var html = '<div class="error_statement">' + str + '</div>';
+        $('body').prepend( html );
     };
 
 
