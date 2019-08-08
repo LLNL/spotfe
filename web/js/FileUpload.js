3ec54c75ddcd4d3551ce05e809a44e7b242f0a8a
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-08-07 17:31:37 -0700

    fix the enter key for when you enter new cali file directories, handle the case where the enter button is pressed or the update button is clicked.
    Former-commit-id: a60d1f44acc72ca005f5c2917357bfc52a5c0635

--- web/js/FileUpload.js
+++ web/js/FileUpload.js
@@ -9,12 +9,18 @@ ST.FileUpload = function() {
                 directory_val: dir
             }
         },
-        template: '<form action="/" method="post">' +
+        template: '<form action="javascript:void(0);" method="post">' +
         '<div class="instructions">Directory containing cali files:</div>' +
-        '<input type="text" class="directory" v-model:value="directory_val"/>' +
+        '<input type="text" class="directory" v-model:value="directory_val" v-on:keyup="keyup( $event )"/>' +
         '<div class="update_button myButton" v-on:click="update( $event )">UPDATE</div>' +
         '</form>',
         methods: {
+            keyup: function(e) {
+
+                if( e.keyCode === 13 ) {
+                    this.update(e);
+                }
+            },
             update: function( event ) {
 
                 var dir = this.directory_val;
