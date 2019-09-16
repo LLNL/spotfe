a7640b32e06cd681ec91abb9e6246627d6c8dd53
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-09-16 11:51:27 -0700

    return count threshold back to 8
    Former-commit-id: 1211c68054e94c7187ae6273a76c6f30ff076127

--- web/ChartCollection.js
+++ web/ChartCollection.js
@@ -101,7 +101,7 @@ ST.ChartCollection = function() {
             spec.counts = counts;
 
             //  keep disabled for now.
-            if( viz === "PieChart" && counts >= 2 ) {
+            if( viz === "PieChart" && counts >= 8 ) {
                 viz = "HorizontalBarChart";
             }
 
