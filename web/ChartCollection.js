9bbd1169a0031795cbcef004051a14fe5b4b5dbe
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-09-18 14:10:23 -0700

    Refactor the compare arguments such that they have their own module now.
    Former-commit-id: 1aa58393b32a70455053c4fed3a671b897fa02e7

--- web/ChartCollection.js
+++ web/ChartCollection.js
@@ -168,7 +168,7 @@ ST.ChartCollection = function() {
         });
 
 
-        render_compare_arguments(the_data[0]);
+        ST.CompareArguments.render(the_data[0]);
 
         //  https://github.com/HamsterHuey/intothevoid.io/blob/master/code/2017/dcjs%20sortable%20table/dcjsSortableTable.html
 
@@ -232,91 +232,6 @@ ST.ChartCollection = function() {
 
         var show = ST.MAX_SHOW <= count ? 'show' : 'hide';
         $('.contain_show_more')[show]();
-    };
-
-
-    var render_compare_arguments = function (fields) {
-
-        var options = "<option value=''></option>";
-        var farr = get_farr(fields);
-
-        for (var x = 0; x < farr.length; x++) {
-
-            var field = farr[x];
-            options += '<option value="' + field + '">' + field + '</option>';
-        }
-
-        //  '<a id="bookmarkme" href="javascript: void(0)" rel="sidebar">Bookmark</a>' +
-        var ht = '<select class="xaxis">' + options + '</select>' +
-            '<select class="groupby">' + options + '</select>' +
-            '<div class="xaxis_label">Xaxis:</div>' +
-            '<div class="groupby_label">Group By:</div>';
-
-        $('.compare_arguments').html(ht);
-
-        $('.compare_arguments .xaxis, .compare_arguments .groupby').unbind('change').bind('change', function () {
-
-            var cla = $(this).attr('class');
-            var val = $(this).val();
-            val = val.replace(/#/, '');
-
-            ST.UrlStateManager.update_url(cla, val);
-        });
-
-        var last_days = ST.Utility.get_param(ST.LAST_DAYS) || "";
-
-        $('.launch_container').html('<div class="launcher">' +
-            '<div class="days_label">launchdate days ago: </div>' +
-            '<input type="text" class="launchdate_days_ago" value="' + last_days + '"/>' +
-            '<div class="launch_button myButton">RELOAD</div>' +
-            '</div>');
-
-        $('.launch_button').unbind('click').bind('click', function () {
-
-            var val = $('.launchdate_days_ago').val();
-            ST.UrlStateManager.update_url("launchdate_days_ago", val);
-            location.reload();
-        });
-
-        load_compare();
-    };
-
-
-    var load_compare = function () {
-
-        var xaxis = ST.Utility.get_param('xaxis', true);
-        var groupby = ST.Utility.get_param('groupby', true);
-
-        if (xaxis !== 'undefined') {
-            $('.compare_arguments .xaxis').val(xaxis);
-        }
-
-        if (groupby !== 'undefined') {
-            $('.compare_arguments .groupby').val(groupby);
-        }
-    };
-
-
-    var get_farr = function (fields) {
-
-        var farr = ST.Utility.to_array(fields);
-
-        farr.sort(function (a, b) {
-
-            a = a.toLowerCase();
-            b = b.toLowerCase();
-
-            if (a < b) {
-                return -1;
-            }
-            if (a > b) {
-                return 1;
-            }
-
-            return 0;
-        });
-
-        return farr;
     };
 
 
