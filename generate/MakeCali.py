7f43e072532decd03b7cfe1ed96f7ca8574a29bd
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-09-20 12:37:12 -0700

    Cali generating python script init
    Former-commit-id: fe2acbd8e367de4a36ee689a1de5c04bfebd40ee

--- generate/MakeCali.py
+++ generate/MakeCali.py
@@ -1,3 +1,22 @@
 #!/usr/global/tools/lorenz/python/narf-env/bin/python
 
-print "hi"
+
+class CaliGen:
+    def make(self):
+
+        template_loc = "/g/g0/pascal/lulesh_gen/template"
+
+        file = open( template_loc, "r" )
+        template = file.read()
+
+        print template
+
+
+
+
+
+
+mycali = CaliGen()
+mycali.make()
+
+
