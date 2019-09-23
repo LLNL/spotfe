e0e30f2a4170ea148f2d49a4478f898ec7c1d732
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-09-23 15:48:23 -0700

    allow number of runs to be passed into the script
    Former-commit-id: b7fe91dd1acd374e32a1e5d179a301b84e4a7327

--- generate/MakeCali.py
+++ generate/MakeCali.py
@@ -3,20 +3,22 @@ from pprint import pprint
 from random import randint
 from random import uniform
 from random import choice
+import sys
+
 
 class CaliGen:
-    def __init__(self):
+    def __init__(self ):
 
         template_loc = "template"
         file = open( template_loc, "r" )
         self.template = file.read()
 
 
-    def make(self):
+    def make(self, make_num ):
 
         output_loc = "/g/g0/pascal/lulesh_gen/"
 
-        for i in range(0,20):
+        for i in range(0, int(make_num)):
 
             index = str(i)
 
@@ -45,6 +47,21 @@ class CaliGen:
             "ITERATIONS": {
                 "range": [0,12000000],
                 "type": "integer"
+            },
+            "EXECUTABLEPATH": {
+                "range": ["/g/g90/boehme3/src/LULESH/build-toss3/lulesh2.0",
+                          "/g/g70/aschwanden/src/LULESH_X/build-20/lulesh3.0",
+                          "/etc/crons",
+                          "/home/bin/",
+                          "/usr/group/boehme/lorenz/lulesh283.2",
+                          "/user/bin/cav/",
+                          "/bin/etc/host",
+                          "/hosts/bin/serv",
+                          "/var/log/serv/bin",
+                          "/bin",
+                          "/g/g90/johnson234/exe/STRIP_HEADER/toss17/impending4.8-3472",
+                          "/g/home/exe-no-exceptions/bin/"],
+                "type": "string"
             }
         }
 
@@ -77,6 +94,6 @@ class CaliGen:
 
 
 mycali = CaliGen()
-mycali.make()
+mycali.make( sys.argv[1] )
 
 
