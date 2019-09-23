27b1162d0541f7bc8d4a84ac957147d651c03b8c
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-09-23 10:31:06 -0700

    iterate through all the stub values and generate random values based on the type of the stub value and then replace it in the template file
    Former-commit-id: cdc1ff6e2ac2da0567ce297da85be03bff82b022

--- generate/MakeCali.py
+++ generate/MakeCali.py
@@ -1,5 +1,6 @@
 #!/usr/global/tools/lorenz/python/narf-env/bin/python
-
+from pprint import pprint
+from random import randint
 
 class CaliGen:
     def __init__(self):
@@ -26,34 +27,39 @@ class CaliGen:
 
         stub_values = {
             "FIGURE_OF_MERIT": {
-                range: [0, 8000],
-                type: "float"
+                "range": [0, 8000],
+                "type": "float"
             },
             "ELAPSED_TIME": {
-                range: [0, 400],
-                type: "float"
+                "range": [0, 400],
+                "type": "float"
             },
             "USER": {
-                range: ["boehme3", "legendre1", "aschwanden1", "chavez35"],
-                type: "string"
+                "range": ["boehme3", "legendre1", "aschwanden1", "chavez35"],
+                "type": "string"
             },
             "ITERATIONS": {
-                range: [0,12000000],
-                type: "integer"
+                "range": [0,12000000],
+                "type": "integer"
             }
         }
 
+
         cali_con = self.template
+
+        #pprint(stub_values)
 
         for key, value in stub_values.items():
 
-            self.dump(value)
-            exit
+            vtype = value['type']
+            range = value["range"]
 
-            if value['type'] == "float":
+            if vtype == "float":
                 val = 2038
-            else:
-                val = 90000
+            elif vtype == "string":
+                val = "string"
+            elif vtype == "integer":
+                val = randint( range[0], range[1] )
 
             cali_con = cali_con.replace( key, str(val) )
 
