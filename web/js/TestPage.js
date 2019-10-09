c8b65d7462bd9b01b0abf916109f0340f78e9ee3
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-10-09 16:23:50 -0700

    add localhost to list of things to test and add 500 and 5000 directories for testing.
    Former-commit-id: bda47b0b8e57a792f07bed046a9fcf820014a213

--- web/js/TestPage.js
+++ web/js/TestPage.js
@@ -1,19 +1,22 @@
 var TEST = function() {
 
-    var num = [15,100,1000,2000,10000,14500,32000,96000];
+    var num = [15,100,500,1000,2000,5000,10000,14500,32000,96000];
     var testList = [];
 
     for( var x in num ) {
 
         var count = num[x];
 
+        var luly = '?sf=/g/g0/pascal/lulesh_gen/' + count;
+
         testList.push({
             id: x,
             page: count,
-            "cz_d2": 'https://lc.llnl.gov/spot/dcvis/?sf=/g/g0/pascal/lulesh_gen/' + count,
-            "rz_d2": 'https://rzlc.llnl.gov/spot/dcvis/?sf=/g/g0/pascal/lulesh_gen/' + count,
-            "cz_l2": 'https://lc.llnl.gov/spot2/?sf=/g/g0/pascal/lulesh_gen/' + count,
-            "rz_l2": 'https://rzlc.llnl.gov/spot2/?sf=/g/g0/pascal/lulesh_gen/' + count,
+            "localhost": "http://localhost:8888/" + luly,
+            "cz_d2": 'https://lc.llnl.gov/spot/dcvis/' + luly,
+            "rz_d2": 'https://rzlc.llnl.gov/spot/dcvis/' + luly,
+            "cz_l2": 'https://lc.llnl.gov/spot2/' + luly,
+            "rz_l2": 'https://rzlc.llnl.gov/spot2/' + luly
         });
     }
 
@@ -27,6 +30,7 @@ var TEST = function() {
         <thead>\
             <tr>\
                 <td>Test Page</td>\
+                <td>Localhost</td>\
                 <td>CZ D2  - Dev</td>\
                 <td>RZ D2  - Dev</td>\
                 <td>CZ L2  - Live</td>\
@@ -37,6 +41,7 @@ var TEST = function() {
         <tbody>\
             <tr v-for="row in testList">\
             <td>{{row.page}}</td>\
+            <td><a v-bind:href="row.localhost" target="_blank">{{row.page}}</a></td>\
             <td><a v-bind:href="row.cz_d2" target="_blank">{{row.page}}</a></td>\
             <td><a v-bind:href="row.rz_d2" target="_blank">{{row.page}}</a></td>\
             <td><a v-bind:href="row.cz_l2" target="_blank">{{row.page}}</a></td>\
