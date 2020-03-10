0b06fba5ff8a455fef950bb2af25865856aef2c3
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-03-10 15:31:11 -0700

    Add user preferences template, so that template is in its own file
    Former-commit-id: d2286aaedc2f3465d64be1505130006ce46ad828

--- web/js/UserPreferences.js
+++ web/js/UserPreferences.js
@@ -45,43 +45,36 @@ ST.UserPreferences = function() {
             return true;
         }
 
-        Vue.component('user-preferences', {
-            data: function() {
-                return {
-                    seen: false,
-                    mcharts: charts
+        $.get("web/Templates/UserPreferences.html", function( pref_html ) {
+
+            Vue.component('user-preferences', {
+                data: function() {
+                    return {
+                        seen: false,
+                        mcharts: charts
+                    }
+                },
+                template: pref_html,
+
+                methods: {
+                    check: function( chart_dimension, val ) {
+
+                        var checked = val.target.checked;
+
+                        update_layout_( chart_dimension, checked );
+
+                        //  Need to reload the whole page otherwise there's no way to dynamically remove columns from table.
+                        ST.ChartCollection.RenderChartCollection(ST.newp, ST.layout_used);
+
+                        //  Persist checkbox changes in localStorage.
+                        ST.graph.setChartVisible( chart_dimension, checked );
+                    }
                 }
-            },
-            template: '<div>\
-            <div class="user_pref_icon icon myButton" v-on:click="seen=(!seen)">\
-            <div class="inner"></div>\
-            </div>\
-            <div class="user_pref_body" v-if="seen">\
-                <div v-for="chart in mcharts" class="prow">\
-                    <input type="checkbox" v-bind:checked="chart.show" v-on:click="check( chart.dimension, $event )"/>\
-                    <span class="title">{{ chart.title }}</span>\
-                </div>\
-            </div>\
-            </div>',
+            });
 
-            methods: {
-                check: function( chart_dimension, val ) {
-
-                    var checked = val.target.checked;
-
-                    update_layout_( chart_dimension, checked );
-
-                    //  Need to reload the whole page otherwise there's no way to dynamically remove columns from table.
-                    ST.ChartCollection.RenderChartCollection(ST.newp, ST.layout_used);
-
-                    //  Persist checkbox changes in localStorage.
-                    ST.graph.setChartVisible( chart_dimension, checked );
-                }
-            }
-        });
-
-        new Vue({
-            "el": "#user_preferences"
+            new Vue({
+                "el": "#user_preferences"
+            });
         });
     };
 
