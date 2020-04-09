d419b1dc9974b25a161227a8ededc0b92c97d02c
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-04-09 10:45:05 -0700

    Move hashCode to its Utilities
    Former-commit-id: 8c598602b07c4b21b1c1dafd527bde74f50fe214

--- web/js/BarChart.js
+++ web/js/BarChart.js
@@ -228,19 +228,6 @@ ST.BarChart = function() {
     };
 
 
-    String.prototype.hashCode = function() {
-
-        var hash = 0, i, chr;
-        if (this.length === 0) return hash;
-        for (i = 0; i < this.length; i++) {
-            chr   = this.charCodeAt(i);
-            hash  = ((hash << 5) - hash) + chr;
-            hash |= 0; // Convert to 32bit integer
-        }
-        return hash;
-    };
-
-
     var get_middling_ = function( v ) {
 
         var spli = v.split('-');
--- web/js/Utility.js
+++ web/js/Utility.js
@@ -228,6 +228,20 @@ ST.Utility = function() {
         return expMatch;
     };
 
+
+    String.prototype.hashCode = function() {
+
+        var hash = 0, i, chr;
+        if (this.length === 0) return hash;
+        for (i = 0; i < this.length; i++) {
+            chr   = this.charCodeAt(i);
+            hash  = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    };
+
+
     var get_colors_ = function( dimension, just_pallet ) {
 
         var pallets = ALL_PALLETES_AVAILABLE;
