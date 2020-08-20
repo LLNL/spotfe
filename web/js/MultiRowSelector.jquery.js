e660e68cfcbb1bdc461a2d57d01a339b9e0475da
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-08-19 20:07:30 -0700

    Implement add row implementation so that add row button adds a row in the last position of the table
    Former-commit-id: 49c56f0ff3c864f8118d280c883bc8ad088fb031

--- web/js/MultiRowSelector.jquery.js
+++ web/js/MultiRowSelector.jquery.js
@@ -12,18 +12,13 @@ $.fn.MultiRowSelector = function() {
 
     var row_ = function() {
 
-        return '<table>' +
-            '<tr class="multi_row">' +
+        return '<tr class="multi_row">' +
             '<td>' + operation_select + '</td>' +
             '<td>' + attributes_select + '</td>' +
             '<td>' +
             ReusableView.button("X", "delete_row", "myButton") +
             '</td>' +
-            '</tr>' +
-            '</table>' +
-            '<div class="center">' +
-            ReusableView.button("ADD ROW", "add_row", "myButton") +
-            '</div>';
+            '</tr>';
     };
 
     var bind_ = function( that ) {
@@ -34,16 +29,35 @@ $.fn.MultiRowSelector = function() {
 
     var add_row_ = function() {
 
+        var mrs = $(this).closest('.multi_row_selector');
+        mrs.find('table').append( row_() );
+    };
+
+    var num_rows_ = function() {
+
+        var mrs = $(this).closest('.multi_row_selector');
+        var count = mrs.find('tr').length;
+
+        return count;
     };
 
     var delete_row_ = function() {
 
+        //  can't delete the last row.
+        if( num_rows_() > 1 ) {
+
+            $(this).closest('.multi_row').remove();
+        }
 
     };
 
     return this.each( function() {
 
-        var html = row_();
+        var html = '<table>' + row_() + '</table>' +
+            '<div class="center">' +
+            ReusableView.button("ADD ROW", "add_row", "myButton") +
+            '</div>';
+
         var that = $(this);
 
         that.html(html);
