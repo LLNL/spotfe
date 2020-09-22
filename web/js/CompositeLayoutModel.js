9511438589a5928834e8790763d4a69700ec1437
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-09-22 14:42:23 -0700

    make composite graphing instance based to allow one chart for each layer composite
    Former-commit-id: 9a5d64811b4396239d027d5f18df76db47a9b956

--- web/js/CompositeLayoutModel.js
+++ web/js/CompositeLayoutModel.js
@@ -2,11 +2,23 @@ ST.CompositeLayoutModel = function() {
 
     var update_composite_ = function() {
 
+        var ch = sqs.layout_used.charts;
 
+        for (var y = 0; y < ch.length; y++) {
+
+            var ch_i = ch[y];
+
+            if (ch_i.composite_layout) {
+
+                update_one_plot_( ch_i.composite_layout, ch_i.dimension );
+            }
+        }
+    };
+
+
+    var update_one_plot_ = function( cl, the_dimension ) {
 
         for( var x=0; x < ST.cali_obj_by_key.length; x++ ) {
-
-            var the_dimension = "multi_elapse_time";
 
             //  TODO: figure out the formula based on what we've saved in the layout.
             ST.cali_obj_by_key[x][the_dimension] = parseInt(Math.random()*90);
