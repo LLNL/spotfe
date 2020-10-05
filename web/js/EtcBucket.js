ba81c1ba7d881c9b6906b574b1a021c1cb62965c
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-10-05 11:09:43 -0700

    document EtcBucket feature
    Former-commit-id: 3465d1a9cd2384425bef3f46c25323b84ba267a5

--- web/js/EtcBucket.js
+++ web/js/EtcBucket.js
@@ -1,3 +1,16 @@
+/*
+EtcBucket is a Performance optimization feature of the Horizontal Bar Charts.  In Cases where there are hundreds or
+thousands of unique values to HorizontalBarChart, dc.js will slow down to an unacceptable level.  Reducing the number
+of Bars to less than say 50 or so, mitigates this.
+
+The EtcBucket module that I wrote, reduces the number of unique values for a given field to a certain constant limit,
+for those fields that contains too many unique values.  This makes the chart perform better.  But, users would still
+like to see some of those unique values.  So, I wrote a caching system which saves those unique values in a separate
+cache.  After DC.js renders all the charts, I have an algorithm which references my cache and starts, asynchronously
+loading those values into the bottom table DOM.  This way, we can have the best of both worlds.  We still get good
+performance and users can still check the table which contains all the unique values.
+
+ */
 ST.EtcBucket = function() {
 
     var make_column_index_ = function( table_spec ) {
