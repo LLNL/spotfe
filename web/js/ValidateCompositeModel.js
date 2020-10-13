568fce05e8c3758d245702c0ba638835f038d6d1
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-10-13 15:09:23 -0700

    recognize a non plus operation
    Former-commit-id: 7a6ed43bc272c34dd8739dfdb39fd4bdc9b73c36

--- web/js/ValidateCompositeModel.js
+++ web/js/ValidateCompositeModel.js
@@ -4,20 +4,27 @@ ST.ValidateCompositeModel = function() {
 
         console.dir( ops );
 
+        var have_at_least_1_str = false;
+        var have_non_plus_op = false;
+
         for( var z = 0; z < ops.length; z++ ) {
 
             var att = ops[z].attribute;
+            var operation = ops[z].operation;
             var type = ST.UserPreferences.get_dimension( att );
 
             console.log(type);
             console.log(att);
 
+            if( type === 'string' ) {
+                have_at_least_1_str = true;
+            }
 
+            if( operation !== undefined && operation !== "+" ) {
+                have_non_plus_op = true;
+            }
         }
 
-        if( ops[1].attribute === "cmdline") {
-            return "its cmdline.";
-        }
 
         //  everything is okay.
         return true;
