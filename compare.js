fe90e4853ad7828a9dc1cc1ea8b8b2c53f44cb5b
Pascal Aschwanden <aschwanden1@llnl.gov>
2021-03-18 17:14:36 -0700

    update compare

--- compare.js
+++ compare.js
@@ -52372,7 +52372,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53655" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50704" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
