e8b380f67ff581ef2a0a8aa5413cdaab5a81d076
Pascal Aschwanden <aschwanden1@llnl.gov>
2021-07-29 11:28:50 -0700

    describe what a custom template substitutions are done and create a pop up modal just for that.  link pop up click to that modal

--- web/js/CustomTemplates.js
+++ web/js/CustomTemplates.js
@@ -35,7 +35,9 @@ ST.CustomTemplates = function() {
             options += "<option>" + json[x] + '</option>';
         }
 
-        var body = "<div class='select_a_jup'>Select a Jupyter notebook: </div>" +
+        var body = "<div class='select_a_jup'>When you select a template, a number of " +
+            "<a class='explanation' href='javascript: void();'>substitutions are made</a>" +
+            "</div>" +
             "<select class='jupyter_notebook'>" + options + '</select>';
 
         ReusableView.modal({
@@ -43,9 +45,27 @@ ST.CustomTemplates = function() {
             header: "Select"
         });
 
+        $('.outer_modal .explanation').unbind('click').bind('click', explanation_);
         $('.outer_modal .jupyter_notebook').change( jupyter_notebook_selected_ );
     };
 
+
+    var explanation_ = function() {
+
+        ReusableView.modal({
+            body: "You may put the following strings inside your template, using an editor.  " +
+                "Once your template is selected from the spot2 dashboard, the following substitutions will be made." +
+                "" +
+                "<h3>Template substitutions:</h3>" +
+                "<table class='list_of_temp'>" +
+                "<tr><td>MUTLI_CALI_FILES</td><td>the runs you select are passed to any multi template you select.</td></tr>" +
+                "<tr><td>CALI_METRIC_NAME</td><td>the first runs 'metric_name' attribute is substitued for this.</td></tr>" +
+                "<tr><td>CALI_QUERY_PATH</td><td>/usr/gapps/spot/caliper-install/bin is subtituted for this string</td></tr>" +
+                "<tr><td>DEPLOY_DIR</td><td>/usr/gapps/spot/ is subtitued for this.</td></tr>" +
+                "</table>",
+            header: "Substitutions"
+        });
+    };
 
     var jupyter_notebook_selected_ = function( el ) {
 
