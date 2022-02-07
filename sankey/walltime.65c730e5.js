222320763b9a2e4c4dcfda40733260929f5ac5a8
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-02-07 10:44:51 -0800

    Package work in order to make npm work better plus a number of file clean ups.  Package.json was cleaned up a bit

--- sankey/walltime.65c730e5.js
+++ sankey/walltime.65c730e5.js
@@ -41303,7 +41303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64654" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64061" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
