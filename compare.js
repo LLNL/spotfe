690dc90afa4ddd6afa71e7504ef98b3892b9d1bf
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-03-15 15:33:48 -0700

    add jquery back in

--- compare.js
+++ compare.js
@@ -52742,7 +52742,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51861" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55812" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
