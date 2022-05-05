e2b1b0809c440aff4fe4b89d6fb9f2c9475b37d1
Pascal Aschwanden <aschwanden1@llnl.gov>
2022-05-05 13:47:43 -0700

    ares setup removal, moved to its own page

--- compare-rebuild/src/index.js
+++ compare-rebuild/src/index.js
@@ -423,42 +423,6 @@ export class Graph{
 
         var app2 = this.app;
         app2.mount( this.selector );
-
-        this.makeAres();
-    }
-
-
-    async makeAres() {
-
-        $('body').append('<div class="ares_container"></div>');
-
-        var selSankeyPath = "main";
-
-        var runData = {};
-        runData[selSankeyPath] = {};
-
-        runData[selSankeyPath].topdown = {
-              retiring: .15,
-              bad_speculation: .8,
-              frontend_bound: .612,
-              backend_bound: .23,
-              branch_mispredict: .9,
-              machine_clear: .11,
-              frontend_latency: .5,
-              frontend_bandwidth: .18,
-              core_bound: .9,
-              memory_bound: .212,
-              l1_bound: .4,
-              l2_bound: .8,
-              l3_bound: .11,
-              uncore_bound: .7,
-              mem_bound: .4
-          };
-
-        const props = reactive({ runData: runData });
-        var aresApp = createApp(AresSankey, props);
-
-        aresApp.mount(".ares_container");
     }
 
 
